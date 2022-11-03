27f08f89dd9836a08a119b7ef095b29454769d9c
David Brouwer <david.brouwer.99@gmail.com>
2022-11-03 03:00:19 +0100

    feat(log/text): add logger (#5)

---
+++ log/text/text.go
@@ -0,0 +1,29 @@
+// Package text provides a the slog text handler.
+package text
+
+import (
+	"os"
+
+	"go-micro.dev/v5/log"
+	"golang.org/x/exp/slog"
+)
+
+func init() {
+	if err := log.Plugins.Add("textstdout", NewHandlerstdout); err != nil {
+		panic(err)
+	}
+
+	if err := log.Plugins.Add("textstderr", NewHandlerStderr); err != nil {
+		panic(err)
+	}
+}
+
+// NewHandlerstdout writes text to stdout.
+func NewHandlerstdout(level slog.Leveler) (slog.Handler, error) {
+	return slog.HandlerOptions{Level: level}.NewTextHandler(os.Stdout), nil
+}
+
+// NewHandlerStderr writes text to stderr.
+func NewHandlerStderr(level slog.Leveler) (slog.Handler, error) {
+	return slog.HandlerOptions{Level: level}.NewTextHandler(os.Stderr), nil
+}
