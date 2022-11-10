d08e534fa23b4c185db1dc42ffb505a901f3e23d
Davincible <david.brouwer.99@gmail.com>
2022-11-10 21:35:07 +0100

    fix(registry/mdns): refactor, linting, options

--- registry/mdns/config.go
+++ registry/mdns/config.go
@@ -1,14 +1,23 @@
 package mdns
 
 import (
+	"fmt"
+
+	"go-micro.dev/v5/config"
+	"go-micro.dev/v5/config/source"
 	"go-micro.dev/v5/config/source/cli"
-	"go-micro.dev/v5/log"
 	"go-micro.dev/v5/registry"
+	"go-micro.dev/v5/types"
 )
 
 const name = "mdns"
 
-var DefaultDomain = "orb"
+// Defaults.
+//
+//nolint:gochecknoglobals
+var (
+	DefaultDomain = "micro"
+)
 
 func init() {
 	//nolint:errcheck
@@ -19,20 +28,40 @@ func init() {
 		cli.Usage("Registry domain."),
 	))
 
-	if err := registry.Plugins.Add(name, Provide); err != nil {
+	if err := registry.Plugins.Add(name, registry.Provider(ProvideRegistryMDNS)); err != nil {
 		panic(err)
 	}
 }
 
-type Config struct {
-	*registry.Config `yaml:",inline"`
-
-	Domain string `json:"domain,omitempty" yaml:"domain,omitempty"`
-	Logger log.Logger
+// WithDomain sets the MDNS domain.
+func WithDomain(domain string) registry.Option {
+	return func(c registry.ConfigType) {
+		cfg, ok := c.(*Config)
+		if ok {
+			cfg.Domain = domain
+		}
+	}
 }
 
-func NewConfig() *Config {
-	return &Config{
+// Config provides configuration for the MDNS registry.
+type Config struct {
+	registry.Config `yaml:",inline"`
+
+	Domain string `json:"domain,omitempty" yaml:"domain,omitempty"`
+}
+
+// NewConfig creates a new config object.
+func NewConfig(serviceName types.ServiceName, datas []source.Data, opts ...registry.Option) (Config, error) {
+	cfg := Config{
 		Config: registry.NewConfig(),
 	}
+
+	cfg.ApplyOptions(opts...)
+
+	sections := types.SplitServiceName(serviceName)
+	if err := config.Parse(append(sections, registry.ComponentType), datas, &cfg); err != nil {
+		return cfg, fmt.Errorf("parse config: %w", err)
+	}
+
+	return cfg, nil
 }
