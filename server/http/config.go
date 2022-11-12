8dab61628ac1612bfcdc0257d559240e3892ce44
Davincible <david.brouwer.99@gmail.com>
2022-11-12 17:13:55 +0100

    feat(server/http): create entrypoint, migrate to core

--- server/http/config.go
+++ server/http/config.go
@@ -1,26 +1,69 @@
 package http
 
 import (
+	"crypto/tls"
 	"errors"
+	"net/http"
+	"time"
 
+	"github.com/go-micro/plugins/server/http/router/router"
+	"github.com/google/uuid"
 	"go-micro.dev/v5/codecs"
 	"go-micro.dev/v5/config"
+	"go-micro.dev/v5/server"
 	"go-micro.dev/v5/types"
 	"go-micro.dev/v5/util/slice"
-
-	"github.com/go-micro/plugins/server/http/entrypoint"
-	"github.com/go-micro/plugins/server/http/router/router"
+	"golang.org/x/exp/slog"
 )
 
-// Default config values.
-//
-//nolint:gochecknoglobals
-var (
-	DefaultAddress        = "0.0.0.0:42069"
-	DefaultRouter         = "chi"
-	DefaultEnableGzip     = false
+var ( //nolint:gochecknoglobals
+	// TODO: revisit default address, probably use random addr
+	// DefaultAddress to use for new HTTP servers.
+	// If set to "random", the default, a random address will be selected,
+	// preferably on a private interface (XX subet). TODO: implement
+	DefaultAddress = "0.0.0.0:42069"
+	// DefaultInsecure will create an HTTP server without TLS, for insecure connections.
+	// Note: as a result you can only make insecure HTTP requests, and no HTTP2
+	// unless you set WithH2C.
+	//
+	// WARNING: don't use this in production, unless you really know what you are doing.
+	// this will result in unencrypted HTTP traffick. Really, it is even advised
+	// against using this in testing.
+	DefaultInsecure = false
+	// DefaultAllowH2C allows insecure, unencrypted traffick to HTTP2 servers.
+	// Don't use this, see the notes at DefaultInsecure for more details.
+	DefaultAllowH2C = false
+	// DefaultMaxConcurrentStreams for HTTP2.
+	DefaultMaxConcurrentStreams = 250
+	// HTTP2 dicates whether to also allow HTTP/2 connections.
+	DefaultHTTP2 = true
+	// HTTP3 dicates whether to also start an HTTP/3.0 server.
+	DefaultHTTP3 = false
+	// DefaultRouter to use as serve mux. There's not really a reason to change this
+	// but if you really wanted to, you could.
+	DefaultRouter = "chi"
+	// DefaultReadTimeout, see net/http pkg for more details.
+	DefaultReadTimeout = 5 * time.Second
+	// DefaultWriteTimeout, see net/http pkg for more details.
+	DefaultWriteTimeout = 5 * time.Second
+	// DefaultIdleimeout, see net/http pkg for more details.
+	DefaultIdleimeout = 5 * time.Second
+	// DefaultEnableGzip enables gzip response compression server wide onall responses.
+	// Only use this if your messages are sufficiently large. For small messages
+	// the compute overhead is not worth the reduction in transport time.
+	//
+	// Alternatively, you can send a gzip compressed request, and the server
+	// will send back a gzip compressed respponse.
+	DefaultEnableGzip = false
+	// DefaultCodecWhitelist is the default allowed list of codecs to be used for
+	// HTTP request encoding/decoding. This means that if any of these plugins are
+	// registered, they will be included in the server's available codecs.
+	// If they are not registered, the server will not be able to handle these formats.
 	DefaultCodecWhitelist = []string{"proto", "jsonpb", "form", "xml"}
-
+	// DefaultEnabled dicates whether the entrypoint is enabled.
+	DefaultEnabled = true
+	// DefaultConfigSection is the section key used in config files used to
+	// configure the server options.
 	DefaultConfigSection = "http"
 )
 
@@ -32,70 +75,132 @@ var (
 	ErrNoMatchingCodecs    = errors.New("no matching codecs found, did you register the codec plugins?")
 )
 
-// Option is a functional HTTP server option.
+// Option is a functional option to provide custom values to the config.
 type Option func(*Config)
 
-// Entrypoint is a list of entrypoint options used to facilitate the creation
-// of one entrypoint, with all options provided. You MUST atleast add the address
-// to listen on WithAddress, and can optionally specify more options to use.
-// For all options not specified, default values will be used.
-type Entrypoint []entrypoint.Option
-
-// Config is the server config. It contains the list of addresses on which
-// entrypoints will be created, and the default config used for each entrypoint.
+// Config provides options to the entrypoint.
 type Config struct {
-	// EntrypointDefaults are the defaults used when creating new entrypoints.
-	EntrypointDefaults entrypoint.Config
-
-	// Entrypoints is the list of entrypoints that will be created.
-	Entrypoints []Entrypoint
-
-	// Router is the router plugin name to use.
-	Router string
-
+	// Name is the entrypoint name.
+	Name string `json:"name" yaml:"name"`
+	// Address to listen on.
+	Address string `json:"address" yaml:"address"`
+	// Insecure will create an HTTP server without TLS, for insecure connections.
+	// Note: as a result you can only make insecure HTTP1 requests, no HTTP2
+	// unless you set WithH2C.
+	//
+	// WARNING: don't use this in production, unless you really know what you are doing.
+	// this will result in unencrypted HTTP traffick. Really, it is even advised
+	// against using this in testing.
+	Insecure bool `json:"insecure" yaml:"insecure"`
+	// MaxConcurrentStreams for HTTP2.
+	MaxConcurrentStreams int `json:"maxConcurrentStreams" yaml:"maxConcurrentStreams"`
+	// TLS config, if none is provided a self-signed certificates will be generated.
+	TLS *tls.Config // TODO: how do add certs from config? add back cerfile/keyfile?
+	// H2C allows h2c connections; HTTP2 without TLS.
+	H2C bool `json:"h2c" yaml:"h2c"`
+	// HTTP2 dicates whether to also allow HTTP/2 connections. Defaults to true.
+	HTTP2 bool `json:"http2" yaml:"http2"`
+	// HTTP3 dicates whether to also start an HTTP/3.0 server. Defaults to false.
+	HTTP3 bool `json:"http3" yaml:"http3"`
+	// Gzip enables gzip response compression server wide onall responses.
+	// Only use this if your messages are sufficiently large. For small messages
+	// the compute overhead is not worth the reduction in transport time.
+	//
+	// Alternatively, you can send a gzip compressed request, and the server
+	// will send back a gzip compressed respponse.
+	Gzip bool `json:"gzip" yaml:"gzip"`
 	// CodecWhitelist is the list of codec names that are allowed to be used
-	// with the HTTP server. We explicitly whitelist codecs, as we don't
+	// with the HTTP server. This means that if registered, codecs in this list
+	// will be added to the server, allowing you to make RPC requests in that format.
+	// If any of the codecs in this list are not registred nothing will happen.
+	//
+	// We explicitly whitelist codecs, as we don't
 	// want to add every codec plugin that has been registered to be automaically
 	// added to the server.
-	//
-	// Adding a codec to this list will mean that if the codec has been registered,
-	// you will be able to make RPC requests in that format.
-	CodecWhitelist []string
-
-	// EnableGzip enables gzip response compression. Only use this if your
-	// messages are sufficiently large. For small messages the compute overhead
-	// is not worth the reduction in transport time.
-	EnableGzip bool
+	CodecWhitelist []string `json:"codecWhitelist" yaml:"codecWhitelist"`
+	// Router is the router plugin to use. Default is chi.
+	Router string `json:"router" yaml:"router"`
+	// ReadTimeout is the maximum duration for reading the entire
+	// request, including the body. A zero or negative value means
+	// there will be no timeout.
+	ReadTimeout time.Duration `json:"readTimeout" yaml:"readTimeout"`
+	// WriteTimeout is the maximum duration before timing out
+	// writes of the response. It is reset whenever a new
+	// request's header is read. Like ReadTimeout, it does not
+	// let Handlers make decisions on a per-request basis.
+	// A zero or negative value means there will be no timeout.
+	WriteTimeout time.Duration `json:"writeTimeout" yaml:"writeTimeout"`
+	// IdleTimeout is the maximum amount of time to wait for the
+	// next request when keep-alives are enabled. If IdleTimeout
+	// is zero, the value of ReadTimeout is used. If both are
+	// zero, there is no timeout.
+	IdleTimeout time.Duration `json:"idleTimeout" yaml:"idleTimeout"`
+	// RegistrationFuncs are all handler registration functions that will be
+	// registered to the server upon startup. You can statically add handlers
+	// By using the fuctional server options. Optionally, you can dynamically
+	// add handlers by registering them to the Handlers global, and setting them
+	// explicitly in the config. TODO: implement
+	RegistrationFuncs []server.RegistrationFunc
+	// Middleware is a list of middleware to use.
+	Middleware []func(http.Handler) http.Handler
+	// TODO: parse list of handlers and middleares from yaml config
+	// Enabled dicates whether an entrypiont is enabled. This useful to dynamically
+	// disable entrypoints through config files.
+	Enabled bool `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Logger  struct {
+		Level  slog.Level `json:"level,omitempty" yaml:"level,omitempty"` // TODO: change with custom level
+		Plugin string     `json:"plugin,omitempty" yaml:"plugin,omitempty"`
+	} `json:"logger" yaml:"logger"`
 }
 
-// NewConfig creates a new server config with default values.
-// To customize the options pas in a list of options.
-func NewConfig(serviceName types.ServiceName, data types.ConfigData, options ...Option) (Config, error) {
+// fileConfig is used to parse the file configs.
+type fileConfig struct {
+	Entrypoints []Config `json:"entrypoints" yaml:"entrypoints"`
+}
+
+func (f *fileConfig) GetConfig(name string) (Config, error) {
+	for _, e := range f.Entrypoints {
+		if name == e.Name {
+			return e, nil
+		}
+	}
+
+	// This should never actually happen.
+	return Config{}, errors.New("entrypoint config not found in file config list")
+}
+
+// NewDefaultConfig will create a new deafult config for the entrypoint.
+func NewDefaultConfig(service types.ServiceName, data types.ConfigData, options ...Option) (Config, error) {
 	cfg := Config{
-		EntrypointDefaults: entrypoint.NewEntrypointConfig(),
-		Entrypoints:        make([]Entrypoint, 0, 1),
-		Router:             DefaultRouter,
-		CodecWhitelist:     DefaultCodecWhitelist,
-		EnableGzip:         DefaultEnableGzip,
+		Name:                 "http-" + uuid.NewString(),
+		Address:              DefaultAddress,
+		Insecure:             DefaultInsecure,
+		MaxConcurrentStreams: DefaultMaxConcurrentStreams,
+		H2C:                  DefaultAllowH2C,
+		HTTP2:                DefaultHTTP2,
+		HTTP3:                DefaultHTTP3,
+		Gzip:                 DefaultEnableGzip,
+		CodecWhitelist:       DefaultCodecWhitelist,
+		Router:               DefaultRouter,
+		ReadTimeout:          DefaultReadTimeout,
+		WriteTimeout:         DefaultWriteTimeout,
+		IdleTimeout:          DefaultIdleimeout,
+		Enabled:              DefaultEnabled,
+		RegistrationFuncs:    make([]server.RegistrationFunc, 5),
+		Middleware:           make([]func(http.Handler) http.Handler, 10),
 	}
 
 	cfg.ApplyOptions(options...)
 
-	// TODO: optimize this. extract this part into a separate function? How can we make config easy
-	sections := types.SplitServiceName(serviceName)
+	sections := types.SplitServiceName(service)
 	if err := config.Parse(append(sections, DefaultConfigSection), data, &cfg); err != nil {
 		return cfg, err
-	}
-
-	if len(cfg.Entrypoints) == 0 {
-		e := Entrypoint{entrypoint.WithAddress(DefaultAddress)}
-		cfg.Entrypoints = append(cfg.Entrypoints, e)
 	}
 
 	return cfg, nil
 }
 
-// ApplyOptions takes a list of options and applies them to the current config.
+// ApplyOptions applies a set of options to the config.
 func (c *Config) ApplyOptions(options ...Option) {
 	for _, option := range options {
 		option(c)
@@ -113,6 +218,7 @@ func (c *Config) NewCodecMap() (codecs.Map, error) {
 
 	for name, codec := range codecs.Plugins.All() {
 		if slice.In(c.CodecWhitelist, name) {
+			// One codec can support multiple mime types, we add all of them to the map.
 			for _, mime := range codec.ContentTypes() {
 				cm[mime] = codec
 			}
@@ -141,97 +247,160 @@ func (c *Config) NewRouter() (router.Router, error) {
 	return newRouter(), nil
 }
 
-// WithEntrypointOptions takes a list of entrypoint.Option to apply to the
-// EntrypointDefaults, to use as defaults when new entrypoints are created.
-// See entrypoint.Config for more details about the possible options.
-func WithEntrypointOptions(options ...entrypoint.Option) Option {
+// parseFileConfig applies the file config options to the config.
+func parseFileConfig(service types.ServiceName, data types.ConfigData, cfg Config) (Config, error) {
+	var err error
+
+	// We store the config we already have, and then overwrite the fields that
+	// have been provided in a file config.
+	fc := fileConfig{
+		Entrypoints: []Config{cfg},
+	}
+
+	sections := types.SplitServiceName(service)
+	if err = config.Parse(append(sections, DefaultConfigSection), data, &fc); err != nil {
+		return cfg, err
+	}
+
+	cfg, err = fc.GetConfig(cfg.Name)
+	if err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+// WithName sets the entrypoint name.
+func WithName(name string) Option {
 	return func(c *Config) {
-		c.EntrypointDefaults.ApplyOptions(options...)
+		c.Name = name
 	}
 }
 
-// WithEntrypointDefaults takes a complete entrypoint.Config to use as default
-// when creating new entrypoints. Only use this if you want to specify (almost)
-// all values in the entrypoint.Config. Otherwise use WithEntrypointOptions to
-// specify a list of options.
-func WithEntrypointDefaults(defaults entrypoint.Config) Option {
+// WithAddress specifies the address to listen on.
+// If you want to listen on all interfaces use the format ":8080"
+// If you want to listen on a specific interface/address use the full IP.
+func WithAddress(address string) Option {
 	return func(c *Config) {
-		c.EntrypointDefaults = defaults
+		c.Address = address
 	}
 }
 
-// WithAddress takes a list of addresses to listen on.
-// This is an alias for WithEntrypoints, specifying only the address.
-//
-// It will create an Entrypoint for each address, attaching an HTTP server on each.
-// Each entrypoint will be created with the Config.DefaultEntryPointConfig.
-// If you want to change the defaults used for the creation of each entrypoint
-// specify them WithEntrypointOptions, if you want to create custom entrypoints
-// with specific non-default options, use WithEntrypoints.
-//
-// To listen on all interfaces on one specific port use the ":<port>" notation.
-// To listen on a specific interface use the "<IP>:<port>" notation.
-func WithAddress(addrs ...string) Option {
+// WithTLSConfig sets a tls config.
+func WithTLSConfig(tlsConfig *tls.Config) Option {
 	return func(c *Config) {
-		for _, addr := range addrs {
-			e := Entrypoint{entrypoint.WithAddress(addr)}
-			c.Entrypoints = append(c.Entrypoints, e)
+		c.TLS = tlsConfig
+	}
+}
+
+// WithInsecure will create the entrypoint without using TLS.
+// Note: as a result you can only make insecure HTTP requests, and no HTTP2
+// unless you set WithH2C.
+//
+// WARNING: don't use this in production, unless you really know what you are doing.
+// this will result in unencrypted HTTP traffick. Really, it is even advised
+// against using this in testing.
+func WithInsecure() Option {
+	return func(c *Config) {
+		c.Insecure = true
+	}
+}
+
+// WithHTTP3 will additionally enable an HTTP3 server on the entrypoint.
+func WithHTTP3() Option {
+	return func(c *Config) {
+		c.HTTP3 = true
+	}
+}
+
+// WithDisableHTTP2 will prevent the creation of an HTTP2 server on the entrypoint.
+func WithDisableHTTP2() Option {
+	return func(c *Config) {
+		c.HTTP2 = false
+	}
+}
+
+// WithEnableGzip enables gzip response compression server wide onall responses.
+// Only use this if your messages are sufficiently large. For small messages
+// the compute overhead is not worth the reduction in transport time.
+//
+// Alternatively, you can send a gzip compressed request, and the server
+// will send back a gzip compressed respponse.
+func WithEnableGzip() Option {
+	return func(c *Config) {
+		c.Gzip = true
+	}
+}
+
+// WithAllowH2C will allow H2C connections on the entrypoint. H2C is HTTP2 without TLS.
+// It is not recommended to turn this on.
+func WithAllowH2C() Option {
+	return func(c *Config) {
+		c.H2C = true
+	}
+}
+
+// WithDefaults sets default options to use on the creattion of new HTTP entrypoints.
+func WithDefaults(options ...Option) server.Option {
+	return func(c *server.Config) {
+		cfg := c.Defaults[Plugin].(Config)
+
+		for _, o := range options {
+			o(&cfg)
+		}
+
+		c.Defaults[Plugin] = cfg
+	}
+}
+
+// WithConfig will set replace the server config with config provided as argument.
+// Warning: any options applied previous to this option will be overwritten by
+// the contents of the config provided here.
+func WithConfig(config Config) Option {
+	return func(c *Config) {
+		*c = config
+	}
+}
+
+// WithRegistrations appends a list of handler registration functions to the
+// server config.
+func WithRegistrations(funcs ...server.RegistrationFunc) Option {
+	return func(c *Config) {
+		c.RegistrationFuncs = append(c.RegistrationFuncs, funcs...)
+	}
+}
+
+// WithMiddleware appends middlewares to the server.
+// You can use any standard Go HTTP middleware.
+func WithMiddleware(middlewares ...func(http.Handler) http.Handler) Option {
+	return func(c *Config) {
+		c.Middleware = append(c.Middleware, middlewares...)
+	}
+}
+
+func WithEntrypoint(options ...Option) server.Option {
+	return func(c *server.Config) {
+		cfgAny, ok := c.Defaults[Plugin]
+		if !ok {
+			// Should never happen, but just in case.
+			panic("no defaults for http entrypoint found")
+		}
+
+		cfg, ok := cfgAny.(Config)
+		if !ok {
+			// Should never happen, but just in case.
+			panic("default config for http entrypoint is not of type http.Config")
+		}
+
+		cfg.ApplyOptions(options...)
+
+		c.Templates[cfg.Name] = server.EntrypointTemplate{
+			Type:   Plugin,
+			Config: cfg,
+			// TODO: maybe remove this field.
+			Options: []server.EntrypointOption{},
 		}
 	}
 }
 
-// WithEntrypoints is used to provide a list of entrypoints to create, will
-// custom options only applied to each entypoint individually.
-// You MUST atleast provide an address to listen on WithAddress.
-//
-// Example:
-//
-//	WithEntrypoints([]http.Entrypoint{
-//	 {entrypoint.WithAddress(":8080")},
-//	 {entrypoint.WithAddress("192.168.1.50:8081"), WithHTTP3()},
-//	 {entrypoint.WithAddress(":8082"), WithInsecure(), WithWriteTimeout(...)},
-//	})
-func WithEntrypoints(entrypoints ...Entrypoint) Option {
-	return func(c *Config) {
-		c.Entrypoints = append(c.Entrypoints, entrypoints...)
-	}
-}
-
-// WithInsecure starts the servers without TLS certificate, use this if you
-// want to server HTTP traffick without encryption.
-func WithInsecure() Option {
-	return WithEntrypointOptions(entrypoint.WithInsecure())
-}
-
-// WithHTTP3 enabled the HTTP3 sever by default on the creation of new entrypoints.
-func WithHTTP3() Option {
-	return WithEntrypointOptions(entrypoint.WithHTTP3())
-}
-
-// WithGzip enables gzip compression on the servers. Only use this if your
-// messages are sufficiently large. For small messages the compute overhead
-// is not worth the reduction in transport time.
-func WithGzip() Option {
-	return func(c *Config) {
-		c.EnableGzip = true
-	}
-}
-
-// WithRouter sets the default router plugin to use.
-// Make sure to import your router plugin.
-func WithRouter(name string) Option {
-	return func(c *Config) {
-		c.Router = name
-	}
-}
-
-// WithDisableHTTP2 disables HTTP2 by default on all newly created entrypoints.
-func WithDisableHTTP2() Option {
-	return WithEntrypointOptions(entrypoint.DisableHTTP2())
-}
-
-// WithAllowH2C enables insecure HTTP2 support; HTTP/2 without TLS.
-// This is thus also only possible if you set WithInsecure.
-func WithAllowH2C() Option {
-	return WithEntrypointOptions(entrypoint.AllowH2C())
-}
+// TODO: other options and option comments
---
+++ server/http/entrypoint.go
@@ -0,0 +1,262 @@
+// Package http provides an HTTP server implementation.
+// It provides an HTTP1, HTTP2, and HTTP3 server, the first two enabled by default.
+//
+// One server contains multiple entrypoints, with one entrypoint being one
+// address to listen on. Each entrypoint with start its own HTTP2 server, and
+// optionally also an HTTP3 server. Each entrypoint can be customized individually,
+// but default options are provided, and can be tweaked.
+//
+// The architecture is based on the Traefik server implementation.
+package http
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+
+	"go-micro.dev/v5/codecs"
+	"go-micro.dev/v5/log"
+	"go-micro.dev/v5/server"
+	"go-micro.dev/v5/types"
+	"go-micro.dev/v5/types/component"
+	"golang.org/x/exp/slog"
+
+	"github.com/go-micro/plugins/server/http/router/router"
+	mip "github.com/go-micro/plugins/server/http/utils/ip"
+	mtcp "github.com/go-micro/plugins/server/http/utils/tcp"
+	mtls "github.com/go-micro/plugins/server/http/utils/tls"
+	mudp "github.com/go-micro/plugins/server/http/utils/udp"
+)
+
+var _ server.Entrypoint = (*ServerHTTP)(nil)
+
+// Plugin is the plugin name.
+const Plugin = "http"
+
+// ServerHTTP represents a listener on one address. You can create multiple
+// entrypoints for multiple addresses and ports. This is e.g. useful if you
+// want to listen on multiple interfaces, or multiple ports in parallel, even
+// with the same handler.
+type ServerHTTP struct {
+	Config Config
+	Logger log.Logger
+
+	router router.Router
+	codecs map[string]codecs.Marshaler
+
+	httpServer  *httpServer
+	http3Server *http3server
+
+	listenerUDP net.PacketConn
+	listenerTCP net.Listener
+
+	started bool
+}
+
+// ProviderServerHTTP creates a new entrypoint for a single address. You can create
+// multiple entrypoints for multiple addresses and ports. One entrypoint
+// can serve a HTTP1, HTTP2 and HTTP3 server. If you enable HTTP3 it will listen
+// on both TCP and UDP on the same port.
+func ProviderServerHTTP(name string, service types.ServiceName, data types.ConfigData, logger log.Logger, c any, options ...Option) (*ServerHTTP, error) {
+	var err error
+
+	cfg, ok := c.(Config)
+	if !ok {
+		return nil, ErrInvalidConfigType
+	}
+
+	cfg.ApplyOptions(options...)
+
+	// Name needs to be explicitly set, as the config may be inherited and contain
+	// a different name.
+	cfg.Name = name
+
+	cfg, err = parseFileConfig(service, data, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = mip.ValidateAddress(cfg.Address); err != nil {
+		return nil, err
+	}
+
+	router, err := cfg.NewRouter()
+	if err != nil {
+		return nil, fmt.Errorf("http server: create router (%s): %v", cfg.Router, err)
+	}
+
+	codecs, err := cfg.NewCodecMap()
+	if err != nil {
+		return nil, fmt.Errorf("http server: create codec map: %v", err)
+	}
+
+	logger, err = logger.WithComponent(server.ComponentType, Plugin, cfg.Logger.Plugin, cfg.Logger.Level)
+	if err != nil {
+		return nil, fmt.Errorf("create %s (http) component logger: %w", name, err)
+	}
+
+	logger = logger.With(slog.String("entrypoint", name))
+
+	entrypoint := ServerHTTP{
+		Config: cfg,
+		Logger: logger,
+		codecs: codecs,
+		router: router,
+	}
+
+	entrypoint.Config.TLS, err = entrypoint.setupTLS()
+	if err != nil {
+		return nil, err
+	}
+
+	entrypoint.httpServer, err = entrypoint.newHTTPServer(router)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create HTTP server: %w", err)
+	}
+
+	if !entrypoint.Config.HTTP3 {
+		return &entrypoint, nil
+	}
+
+	entrypoint.http3Server, err = entrypoint.newHTTP3Server()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create HTTP3 server: %w", err)
+	}
+
+	return &entrypoint, nil
+}
+
+// Start will create the listeners and start the server on the entrypoint.
+func (e *ServerHTTP) Start() error {
+	if e.started {
+		return nil
+	}
+
+	// TODO: register middlware and handlers
+
+	var err error
+
+	e.Logger.Debug("Starting all HTTP entrypoints")
+
+	e.listenerTCP, err = mtcp.BuildListenerTCP(e.Config.Address, e.Config.TLS)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		if err = e.httpServer.Start(e.listenerTCP); err != nil {
+			e.Logger.Error("Failed to start HTTP server", err)
+		}
+	}()
+
+	if !e.Config.HTTP3 {
+		e.started = true
+		return nil
+	}
+
+	// Listen on the same UDP port as TCP for HTTP3
+	e.listenerUDP, err = mudp.BuildListenerUDP(e.Config.Address)
+	if err != nil {
+		return fmt.Errorf("failed to start UDP listener: %w", err)
+	}
+
+	go func() {
+		if err := e.http3Server.Start(e.listenerUDP); err != nil {
+			e.Logger.Error("Failed to start HTTP3 server", err)
+		}
+	}()
+
+	e.started = true
+
+	return nil
+}
+
+type stopper interface {
+	Stop(context.Context) error
+}
+
+// Stop will stop the HTTP server(s).
+func (e *ServerHTTP) Stop(ctx context.Context) error {
+	if !e.started {
+		return nil
+	}
+
+	errChan := make(chan error)
+	defer close(errChan)
+
+	e.Logger.Debug("Stopping all HTTP entrypoints")
+
+	c := 1
+	if e.Config.HTTP3 {
+		c++
+
+		go func() {
+			errChan <- e.http3Server.Stop(ctx)
+
+			// Listener most likely already closed, just as a double check.
+			_ = e.listenerUDP.Close() //nolint:errcheck
+		}()
+	}
+
+	go func(srv stopper, l net.Listener) {
+		errChan <- srv.Stop(ctx)
+
+		// Listener most likely already closed, just as a double check.
+		_ = l.Close() //nolint:errcheck
+	}(e.httpServer, e.listenerTCP)
+
+	var err error
+
+	for i := 0; i < c; i++ {
+		if nerr := <-errChan; nerr != nil {
+			err = nerr
+		}
+	}
+
+	e.started = false
+
+	return err
+}
+
+func (e *ServerHTTP) Register(register server.RegistrationFunc) {
+	register(e)
+}
+
+func (e *ServerHTTP) String() string {
+	return Plugin
+}
+
+func (e *ServerHTTP) Name() string {
+	return e.Config.Name
+}
+
+func (e *ServerHTTP) Type() component.Type {
+	return server.ComponentType
+}
+
+func (e *ServerHTTP) Router() router.Router {
+	return e.router
+}
+
+func (e *ServerHTTP) setupTLS() (*tls.Config, error) {
+	var (
+		config *tls.Config
+		err    error
+	)
+
+	// TLS already provided
+	if e.Config.TLS != nil {
+		return e.Config.TLS, nil
+	}
+
+	// Generate self signed cert
+	if !e.Config.Insecure && e.Config.TLS == nil {
+		config, err = mtls.GenTlSConfig(e.Config.Address)
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate self signed certificate: %w", err)
+		}
+	}
+
+	return config, nil
+}
