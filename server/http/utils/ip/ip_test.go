3455aadb185bb0e6a406afda1b3d0f80d64de8f6
Davincible <david.brouwer.99@gmail.com>
2022-11-15 00:48:41 +0100

    fix(server/http): review comments

--- server/http/utils/ip/ip_test.go
+++ server/http/utils/ip/ip_test.go
@@ -22,6 +22,8 @@ var tests = []struct {
 	{"8080:", true},
 	{"", true},
 	{":abc", true},
+	{"[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:8080", false},
+	{"[2001:db8::1]:8080", false},
 }
 
 func TestIPParser(t *testing.T) {
