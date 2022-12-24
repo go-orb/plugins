0fee4762f9f2b41d1a96368816d2c2a1a49bc781
René Jochum <rene@jochum.dev>
2022-12-24 13:27:52 +0100

    fix(registry/consul): fix download script

--- scripts/deps/consul.sh
+++ scripts/deps/consul.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd registry/consul/test/bin/linux_amd64 || exit 1
 
-if [ -e consul ]; then
+if [ ! -x consul ]; then
     wget -q -O consul.zip https://releases.hashicorp.com/consul/1.14.3/consul_1.14.3_linux_amd64.zip
     unzip consul.zip
 fi
