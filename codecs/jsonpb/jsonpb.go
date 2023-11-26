01e05cbbb6d11ea951a6f4119fcbc521aba43d89
René Jochum <rene@jochum.dev>
2023-11-26 20:20:03 +0100

    feat(codecs/jsonpb): use protojson instead of grpc-gateway as encoder.

--- codecs/jsonpb/jsonpb.go
+++ codecs/jsonpb/jsonpb.go
@@ -6,7 +6,9 @@ import (
 	"io"
 
 	"github.com/go-orb/go-orb/codecs"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 var _ codecs.Marshaler = (*JSONPb)(nil)
@@ -15,20 +17,57 @@ func init() {
 	codecs.Register("jsonpb", &JSONPb{})
 }
 
+type mEncoder struct {
+	w io.Writer
+}
+
+func (m *mEncoder) Encode(v any) error {
+	b, err := protojson.Marshal(v.(protoreflect.ProtoMessage))
+	if err != nil {
+		return err
+	}
+
+	_, err = m.w.Write(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type mDecoder struct {
+	r io.Reader
+}
+
+func (m *mDecoder) Decode(v any) error {
+	b, err := io.ReadAll(m.r)
+	if err != nil {
+		return err
+	}
+
+	return protojson.Unmarshal(b, v.(protoreflect.ProtoMessage))
+}
+
 // JSONPb wraps Google's implementation of a JSON <> Protocol buffer marshaller
 // that has more extented support for protocol buffer fields.
-type JSONPb struct {
-	runtime.JSONPb
+type JSONPb struct{}
+
+func (j *JSONPb) Marshal(v any) ([]byte, error) {
+	return protojson.Marshal(v.(protoreflect.ProtoMessage))
+}
+
+func (j *JSONPb) Unmarshal(b []byte, v any) error {
+	return protojson.Unmarshal(b, v.(protoreflect.ProtoMessage))
 }
 
 // NewEncoder returns a new JSON/ProtocolBuffer encoder.
 func (j *JSONPb) NewEncoder(w io.Writer) codecs.Encoder {
-	return j.JSONPb.NewEncoder(w)
+	return &mEncoder{w: w}
 }
 
 // NewDecoder returns a new JSON/ProtocolBuffer decoder.
 func (j *JSONPb) NewDecoder(r io.Reader) codecs.Decoder {
-	return j.JSONPb.NewDecoder(r)
+	return &mDecoder{r: r}
 }
 
 // ContentTypes returns the content types the marshaller can handle.
