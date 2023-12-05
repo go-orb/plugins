f294ff811467333d4025495b7c955de0ba2593ba
René Jochum <rene@jochum.dev>
2023-12-05 04:38:45 +0100

    feat(benchmarks/client): Rename generate.go -> gen.go.

--- client/tests/proto/gen.go
+++ client/tests/proto/gen.go
@@ -6,4 +6,4 @@ package proto
 //go:generate wget -q -O google/api/http.proto https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto
 
 // Generate proto files
-//go:generate protoc -I . --go-grpc_out=paths=source_relative:. --go-micro-http_out=paths=source_relative:. --go_out=paths=source_relative:. ./echo.proto
+//go:generate protoc -I . --go-grpc_out=paths=source_relative:. --go-micro-http_out=paths=source_relative:. --go_out=paths=source_relative:. --go-drpc_out=paths=source_relative:. ./echo.proto
