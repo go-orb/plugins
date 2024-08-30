c0e4a9753ecde8dad2a091bfeb00642fda112778
René Jochum <rene@jochum.dev>
2024-08-30 06:56:23 +0200

    fix(benchmarks): Remove them as they now live in examples.

--- benchmarks/rps/proto/gen.go
+++
@@ -1,4 +0,0 @@
-// Package proto ...
-package proto
-
-//go:generate protoc -I . --go-orb_out=paths=source_relative:. --go-orb_opt="supported_servers=drpc;grpc;http;hertz" ./echo/echo.proto
--- benchmarks/rps_no_hertz/proto/gen.go
+++
@@ -1,5 +0,0 @@
-// Package proto ...
-package proto
-
-// Generate proto files
-//go:generate protoc -I . --orb_out=paths=source_relative:. --go-orb_opt="supported_servers=drpc;grpc;http" ./echo/echo.proto
