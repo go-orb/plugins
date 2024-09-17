0cb00bbe5e914e407a16cb3498456a1a0531a7be
René Jochum <rene@jochum.dev>
2024-09-17 10:08:04 +0200

    feat: move MDNS and NATS registry to experimental

--- registry/mdns/util/util.go
+++
@@ -1,9 +0,0 @@
-// Package util provides utilities.
-package util
-
-import "strings"
-
-// TrimDot is used to trim the dots from the start or end of a string.
-func TrimDot(s string) string {
-	return strings.Trim(s, ".")
-}
