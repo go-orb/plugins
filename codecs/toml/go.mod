e4f627b623e0b158a01b55a242a42dc92145f913
Koopa <koopapapa@gmail.com>
2025-03-19 11:59:00 +0800

    Add TOML codec implementation

---
+++ codecs/toml/go.mod
@@ -0,0 +1,10 @@
+module github.com/go-orb/plugins/codecs/toml
+
+go 1.23.6
+
+require (
+	github.com/go-orb/go-orb v0.2.1
+	github.com/pelletier/go-toml/v2 v2.2.3
+)
+
+require github.com/cornelk/hashmap v1.0.8 // indirect
---
+++ codecs/toml/toml.go
@@ -0,0 +1,88 @@
+// Package toml implements a TOML Marshaler.
+package toml
+
+import (
+	"io"
+
+	"github.com/go-orb/go-orb/codecs"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+var _ codecs.Marshaler = (*Toml)(nil)
+
+func init() {
+	codecs.Register("toml", &Toml{})
+}
+
+// Toml implements the codecs.Marshaler interface. It can be used to encode/decode
+// toml files, or web requests.
+type Toml struct{}
+
+// Encode encodes "v" into byte sequence.
+func (t *Toml) Encode(v any) ([]byte, error) {
+	return toml.Marshal(v)
+}
+
+// Decode decodes "data" into "v".
+// "v" must be a pointer value.
+func (t *Toml) Decode(data []byte, v any) error {
+	return toml.Unmarshal(data, v)
+}
+
+// NewEncoder returns an Encoder which writes bytes sequence into "w".
+func (t *Toml) NewEncoder(w io.Writer) codecs.Encoder {
+	encoder := toml.NewEncoder(w)
+	return codecs.EncoderFunc(func(v any) error {
+		return encoder.Encode(v)
+	})
+}
+
+// NewDecoder returns a Decoder which reads byte sequence from "r".
+func (t *Toml) NewDecoder(r io.Reader) codecs.Decoder {
+	decoder := toml.NewDecoder(r)
+	return codecs.DecoderFunc(func(v any) error {
+		return decoder.Decode(v)
+	})
+}
+
+// Encodes returns if this codec is able to encode the given type.
+func (t *Toml) Encodes(v any) bool {
+	switch v.(type) {
+	case []string:
+		return true
+	case []byte:
+		return true
+	case []any:
+		return true
+	case map[string]any:
+		return true
+	case string:
+		return true
+	default:
+		return false
+	}
+}
+
+// Decodes returns if this codec is able to decode the given type.
+func (t *Toml) Decodes(v any) bool {
+	return t.Encodes(v)
+}
+
+// ContentTypes returns the content types the marshaller can handle.
+func (t *Toml) ContentTypes() []string {
+	return []string{
+		"application/toml",
+		"text/toml",
+	}
+}
+
+// String returns the codec name.
+func (t *Toml) String() string {
+	return "toml"
+}
+
+// Exts returns the common file extensions for this encoder.
+func (t *Toml) Exts() []string {
+	return []string{".toml", ".tml"}
+}
